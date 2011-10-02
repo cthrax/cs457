5ac9d8cf73ca069f4e0e5810c8a6d7621b69bc4e
myles.bostwick <myles.bostwick@schneider-electric.com>
2011-10-01 18:40:17 -0600

    Fix a C99 error.

--- a2/awget.c
+++ a2/awget.c
@@ -293,66 +293,66 @@ int main(int argc, char *argv[]) {
 }
 
 #if defined(__APPLE__)
-ssize_t
-getline(char **linep, size_t *np, FILE *stream)
-{
-  char *p = NULL;
-  size_t i = 0;
+ssize_t getline(char **linep, size_t *np, FILE *stream) {
+    char *p = NULL;
+    size_t i = 0;
+    int ch = 0;
 
-  if (!linep || !np) {
-    errno = EINVAL;
-    return -1;
-  }
-
-  if (!(*linep) || !(*np)) {
-    *np = 120;
-    *linep = (char *)malloc(*np);
-    if (!(*linep)) {
-      return -1;
+    if (!linep || !np) {
+        errno = EINVAL;
+        return -1;
     }
-  }
 
-  flockfile(stream);
+    if (!(*linep) || !(*np)) {
+        *np = 120;
+        *linep = (char *) malloc(*np);
+        if (!(*linep)) {
+            return -1;
+        }
+    }
 
-  p = *linep;
-  for (int ch = 0; (ch = getc_unlocked(stream)) != EOF;) {
+    flockfile(stream);
+
+    p = *linep;
+    for (ch = 0; (ch = getc_unlocked(stream)) != EOF;) {
+        if (i > *np) {
+            /* Grow *linep. */
+            size_t m = *np * 2;
+            char *s = (char *) realloc(*linep, m);
+
+            if (!s) {
+                int error = errno;
+                funlockfile(stream);
+                errno = error;
+                return -1;
+            }
+
+            *linep = s;
+            *np = m;
+        }
+
+        p[i] = ch;
+        if ('\n' == ch)
+            break;
+        i += 1;
+    }
+    funlockfile(stream);
+
+    /* Null-terminate the string. */
     if (i > *np) {
-      /* Grow *linep. */
-      size_t m = *np * 2;
-      char *s = (char *)realloc(*linep, m);
+        /* Grow *linep. */
+        size_t m = *np * 2;
+        char *s = (char *) realloc(*linep, m);
 
-      if (!s) {
-        int error = errno;
-        funlockfile(stream);
-        errno = error;
-        return -1;
-      }
+        if (!s) {
+            return -1;
+        }
 
-      *linep = s;
-      *np = m;
+        *linep = s;
+        *np = m;
     }
 
-    p[i] = ch;
-    if ('\n' == ch) break;
-    i += 1;
-  }
-  funlockfile(stream);
-
-  /* Null-terminate the string. */
-  if (i > *np) {
-    /* Grow *linep. */
-      size_t m = *np * 2;
-      char *s = (char *)realloc(*linep, m);
-
-      if (!s) {
-        return -1;
-      }
-
-      *linep = s;
-      *np = m;
-  }
-
-  p[i + 1] = '\0';
-  return ((i > 0)? i : -1);
+    p[i + 1] = '\0';
+    return ((i > 0) ? i : -1);
 }
 #endif
