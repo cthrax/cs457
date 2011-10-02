5187db837c8a6a19285f6287203e491326349c9b
myles.bostwick <myles.bostwick@schneider-electric.com>
2011-10-01 18:28:08 -0600

    Add some extraneous code to make it compile on the old version of gcc that OSX uses.

--- a2/awget.c
+++ a2/awget.c
@@ -14,6 +14,81 @@
 #include <arpa/inet.h>
 #include <time.h>
 
+#if defined(__APPLE__)
+#include <stdio.h>   /* flockfile, getc_unlocked, funlockfile */
+#include <stdlib.h>  /* malloc, realloc */
+
+#include <errno.h>   /* errno */
+#include <unistd.h>  /* ssize_t */
+
+ssize_t getline(char **lineptr, size_t *n, FILE *stream);
+
+/* PASTE REMAINDER AT BOTTOM OF FILE */
+ssize_t
+getline(char **linep, size_t *np, FILE *stream)
+{
+  char *p = NULL;
+  size_t i = 0;
+  int ch = 0;
+
+  if (!linep || !np) {
+    errno = EINVAL;
+    return -1;
+  }
+
+  if (!(*linep) || !(*np)) {
+    *np = 120;
+    *linep = (char *)malloc(*np);
+    if (!(*linep)) {
+      return -1;
+    }
+  }
+
+  flockfile(stream);
+
+  p = *linep;
+  for (ch = 0; (ch = getc_unlocked(stream)) != EOF;) {
+    if (i > *np) {
+      /* Grow *linep. */
+      size_t m = *np * 2;
+      char *s = (char *)realloc(*linep, m);
+
+      if (!s) {
+        int error = errno;
+        funlockfile(stream);
+        errno = error;
+        return -1;
+      }
+
+      *linep = s;
+      *np = m;
+    }
+
+    p[i] = ch;
+    if ('\n' == ch) break;
+    i += 1;
+  }
+  funlockfile(stream);
+
+  /* Null-terminate the string. */
+  if (i > *np) {
+    /* Grow *linep. */
+      size_t m = *np * 2;
+      char *s = (char *)realloc(*linep, m);
+
+      if (!s) {
+        return -1;
+      }
+
+      *linep = s;
+      *np = m;
+  }
+
+  p[i + 1] = '\0';
+  return ((i > 0)? i : -1);
+}
+#endif
+
 struct ss_packet
 {
 		uint8_t version;
