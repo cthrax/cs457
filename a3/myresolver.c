434f79f690d304efa8fdcd383d1c81824e9cfc6e
myles.bostwick <myles.bostwick@schneider-electric.com>
2011-12-05 17:51:24 -0700

    Maybe this I'll fix the memory issue.

--- a3/myresolver.c
+++ a3/myresolver.c
@@ -971,6 +971,7 @@ int addToStack(char *name, char* ip) {
         struct STACK_ELE **newStack = (struct STACK_ELE**) malloc(sizeof(struct STACK_ELE *) * (stackSize + 1));
         struct STACK_ELE *newMatch = (struct STACK_ELE*) malloc(sizeof(struct STACK_ELE));
         newMatch->name = malloc(sizeof(char) * strlen(name) + 1);
+        newMatch->ipSize = 1;
         memcpy(newMatch->name, name, strlen(name));
 
         for (i=0; i < stackSize; i++) {
